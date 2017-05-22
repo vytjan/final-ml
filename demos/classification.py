188541c843242b42e5ec2610b7285f905eb00aad
Vytautas Jankauskas <vytas00@gmail.com>
2017-05-22 12:21:24 +0300

    Learning data set in progress.

--- demos/classification.py
+++ demos/classification.py
@@ -17,7 +17,7 @@ def testing():
 	global responses
 	global samples
 	kernel = np.ones((2,2),np.uint8)
-	img = cv2.imread('learn41.png')
+	img = cv2.imread('learn51.png')
 	newx,newy = img.shape[1],img.shape[0]     #new size (w,h)
 	print("Rescaled, new dimensions: ", newx, newy)
 	newimage = cv2.resize(img,(int(newx), int(newy)))
@@ -114,7 +114,7 @@ def adjustWidth(coords, eroded, maxHeight):
 	sampleWidth = 16
 	# positions: a j N J. Default - a
 	heightPos = 1
-	while sampleWidth < 50 and x+sampleWidth < contourWidth + 5 and x >= 0 and y >=0:
+	while sampleWidth < 60 and x+sampleWidth < contourWidth + 5 and x >= 0 and y >=0:
 		# here we should find the nearest neighbours, thickering the width +2px every iteration:
 				# here I need to put the contour of the letter to the center of the 30x10:
 		
